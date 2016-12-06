bada195dfb696696a66f464f9c8b5443cff28d31
Akash Khan <akashkhan25@gmail.com>
2016-12-06 11:03:21 +0800

    Added destroy user action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,10 +4,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:test)
-    @other_user = users(:admin)
+    @admin_user = users(:admin)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
@@ -25,14 +25,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect edit when logged in as wrong user' do
-    log_in_as(@other_user)
+    log_in_as(@admin_user)
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
 
   test 'should redirect update when logged in as wrong user' do
-    log_in_as(@other_user)
+    log_in_as(@admin_user)
     patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
     assert flash.empty?
     assert_redirected_to root_url
@@ -41,6 +41,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test 'should not allow admin attribute to be modified via web' do
+    log_in_as @user
+    assert_not @user.admin?
+    patch user_path(@user), params: { user: { admin: true } }
+    assert_not @user.reload.admin?
   end
 
 end
