26439d0334567c44236fdae6cde877ef5427f012
Akash Khan <akashkhan25@gmail.com>
2016-12-07 18:01:12 +0800

    Added create action for micropost
     - Fixed user edit page not using partials

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,22 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @post = current_user.microposts.build(post_params)
+    if @post.save
+      flash[:success] = 'Post created'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+    
+    def post_params
+      params.require(:micropost).permit(:content)
+    end
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @post = current_user.microposts.build if logged_in?
   end
 
   def help
