012e438cc07beac8b3d51d1100562551c126dd76
Akash Khan <akashkhan25@gmail.com>
2016-12-07 17:21:13 +0800

    Added micropost controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :account_activations, only: [:edit]
+  resources :microposts, only: [:create, :destroy]
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @post = microposts(:random_post)
+  end
+
+  test 'should redirect create when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: 'Random text' } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@post)
+    end
+    assert_redirected_to login_url
+  end
 end
